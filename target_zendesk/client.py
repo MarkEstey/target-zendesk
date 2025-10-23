d9d3506bfe54586b0ed24bb61ebc9eaf942f4c79
Mark Estey <mestey@varicent.com>
2025-10-23 14:19:47 -0400

    fix quoting

--- target_zendesk/client.py
+++ target_zendesk/client.py
@@ -10,12 +10,12 @@ class ZendeskSink(RecordSink):
         self._requests_session = requests.Session()
 
         if self.config.get('api_username') is not None and self.config.get('api_token') is not None:
-            credentials = f'{self.config['api_username']}/token:{self.config['api_token']}'.encode()
+            credentials = f"{self.config['api_username']}/token:{self.config['api_token']}".encode()
             auth_token = base64.b64encode(credentials).decode('ascii')
             self._requests_session.headers.update({'Authorization': f'Basic {auth_token}'})
 
         elif self.config.get('oauth_token') is not None:
-            self._requests_session.headers.update({'Authorization': f'Bearer {self.config['oauth_token']}'})
+            self._requests_session.headers.update({'Authorization': f"Bearer {self.config['oauth_token']}"})
 
         else:
             raise ValueError('Either api_username and api_token or oauth_token must be set for authentication')
